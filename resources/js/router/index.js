c226c0e0fd3f95441738ef53d8a562adefe5885e
hoangkhacphuc <hoangkhacphuc.dev@gmail.com>
2024-09-08 14:04:40 +0700

    update: setup route api

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,6 @@
 import {
     createRouter,
-    createMemoryHistory,
+    createWebHistory,
 } from "vue-router";
 import {
     Index,
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 });
 
